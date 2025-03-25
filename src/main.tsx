1aa75bf569c30322a9d571228695c610291ef56e
raaschdev <devraasch@gmail.com>
2025-03-25 18:07:35 -0300

    chore: Configure my APP

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import MainRoutes from './routes'
+import './index.scss'
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <StrictMode>
+      <MainRoutes />
+    </StrictMode>,
+  </BrowserRouter>
+)
---
+++ src/routes.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import App from "./App";
+
+function MainRoutes() {
+    return (
+        <Routes>
+            <Route element={<App />} path="/" />
+        </Routes>
+    )
+}
+export default MainRoutes;
